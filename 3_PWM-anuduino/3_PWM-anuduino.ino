10dae9e37a1e337afed01e6cab4d9f3b438145a0
Nivedita <rtniveditait@gmail.com>
2014-04-07 12:52:55 +0530

    added gitignore and 3_pwm source file with fzz file too

---
+++ 3_PWM-anuduino/3_PWM-anuduino.ino
@@ -0,0 +1,17 @@
+
+int ledPin = 1;      // LED connected to port 1
+int analogPin = 2;   // potentiometer connected to adc 2
+int val = 0;         // variable to store the read value
+int aref = 0;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);   // sets the pin as output
+}
+
+void loop()
+{
+  val = analogRead(analogPin);   // read the input pin
+  
+  analogWrite(ledPin,val/4);
+}
